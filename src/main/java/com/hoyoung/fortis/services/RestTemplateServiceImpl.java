e625af2cd81d0d556306955f427c0c25ece6baea
sclin0323 <sclin0323@gmail.com>
2023-09-01 16:55:10 +0800

    update

--- src/main/java/com/hoyoung/fortis/services/RestTemplateServiceImpl.java
+++ src/main/java/com/hoyoung/fortis/services/RestTemplateServiceImpl.java
@@ -53,8 +53,6 @@ public class RestTemplateServiceImpl implements RestTemplateService {
 	// For Config User Device
 	@Override
 	public PythonResponse editConfigUserDevice(String deviceName, String macAddress) {
-		//String command = "conf vdom \n edit wireless-0 \n config user device \n edit " + deviceName + " \n set mac "
-		//		+ macAddress + " \n next \n end \n";
 		
 		String command = "config firewall address \n edit "+deviceName+" \n set type mac \n set macaddr "+macAddress+" \n end \n";
 
@@ -64,9 +62,6 @@ public class RestTemplateServiceImpl implements RestTemplateService {
 	@Override
 	public PythonResponse appendConfigUserDeviceGroups(String deviceName, String deviceGroup) {
 
-		//String command = "conf vdom \n edit wireless-0 \n config user device-group \n edit " + deviceGroup
-		//		+ " \n append member " + deviceName + " \n next \n end \n end \n exit \n";
-
 		String command = "config firewall addrgrp \n edit "+deviceGroup+" \n append member "+deviceName+" \n end \n";
 
 		return sendRequestToFortinet(command);
@@ -74,8 +69,6 @@ public class RestTemplateServiceImpl implements RestTemplateService {
 
 	@Override
 	public PythonResponse unselectConfigUserDeviceGroups(String deviceName, String deviceGroup) {
-		//String command = "conf vdom \n edit wireless-0 \n config user device-group \n edit " + deviceGroup
-		//		+ " \n unselect member " + deviceName + " \n next \n end \n end \n exit \n";
 
 		String command = "config firewall addrgrp \n edit "+deviceGroup+" \n unselect member "+deviceName+" \n end \n";
 
@@ -84,8 +77,6 @@ public class RestTemplateServiceImpl implements RestTemplateService {
 
 	@Override
 	public PythonResponse deleteConfigUserDevice(String deviceName) {
-		//String command = "conf vdom \n edit wireless-0 \n config user device \n delete " + deviceName
-		//		+ " \n end \n end \n exit";
 
 		String command = "config firewall address \n delete " + deviceName + " \n end \n";
 
